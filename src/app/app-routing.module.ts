2aa51eb6ebd41abefd53bfb41b2d2b80e9ef3d88
Jakub Szafraniec <kuba.szafraniec@gmail.com>
2019-02-17 22:37:26 +0100

    2.1-lazy-loaded-routes-with-preload-strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import {Routes, RouterModule, PreloadAllModules, PreloadingStrategy, Route} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard/dashboard.component';
 import {FormComponent} from './form/form/form.component';
+import {NEVER, Observable} from 'rxjs';
 
 const routes: Routes = [
   {path: 'dashboard', component: DashboardComponent},
@@ -10,9 +11,20 @@ const routes: Routes = [
   {path: 'legacy', loadChildren: './angular-js-legacy/angular-js-legacy.module#AngularJsLegacyModule'}
 ];
 
+export class PreloadLoremIpsumRouteStrategy implements PreloadingStrategy {
+  preload(route: Route, load: Function): Observable<any> {
+    if (route.path === 'lorem-ipsum') {
+      return load();
+    } else {
+      return NEVER;
+    }
+  }
+}
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadLoremIpsumRouteStrategy})],
+  exports: [RouterModule],
+  providers: [PreloadLoremIpsumRouteStrategy]
 })
 export class AppRoutingModule {
 }
